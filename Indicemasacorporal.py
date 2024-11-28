2b2c9b9c22a5e7f2fedc30af15888e0068b28499
SaritaUsma <sariusma35@hotmail.com>
2024-11-28 16:29:48 -0500

    Changes to be committed: 	modified:   Indicemasacorporal.py

--- Indicemasacorporal.py
+++ Indicemasacorporal.py
@@ -1,4 +1,5 @@
 peso = float(input("ingrese su peso en kg"))
 estatura = float(input("ingrese su estatura en m"))
-IMC = peso//estatura
-
+estatura2 = estatura ** 2
+imc = peso//estatura2
+print("Tu masa corporal es",imc)
